42041dbfed6a19ea3024080f457d9a41166f5795
Manoj Bamdhamravuri <manojb@euler.wacc.wisc.edu>
2015-12-10 00:12:42 -0600

    transpose optimized

--- project/mat_transpose/gpu_transpose_kernel.cuh
+++ project/mat_transpose/gpu_transpose_kernel.cuh
@@ -1,8 +1,8 @@
 __global__ void transpose_kernel(int* sum, int *trans_sum, int* sqsum, int *trans_sqsum, int w, int h)
 {
 
-     volatile __shared__ int sum_temp[16][16];
-     volatile __shared__ int sqsum_temp[16][16];
+     volatile __shared__ int sum_temp[16][17];
+     volatile __shared__ int sqsum_temp[16][17];
 
      int tx = threadIdx.x;
      int ty = threadIdx.y;
@@ -12,17 +12,21 @@ __global__ void transpose_kernel(int* sum, int *trans_sum, int* sqsum, int *tran
      //int t_offset = ty * w + tx;
      int row = by * 16 + ty ;
      int col = bx * 16 + tx;
-    //condition for row and column bound check
-     
+     //condition for row and column bound check
+
      sum_temp[ty][tx] = (row < h && col < w) ? sum[row * w + col] : 0;
      sqsum_temp[ty][tx] = (row < h && col < w) ? sqsum[row * w + col] : 0;
 
+
      __syncthreads();
+
+     row = by * 16 + tx;
+     col = bx * 16 + ty;
 
      if (row < h && col < w)
      {
-     trans_sum[col * h + row] = sum_temp[ty][tx];
-     trans_sqsum[col * h + row] = sqsum_temp[ty][tx];
+          trans_sum[col * h + row] = sum_temp[tx][ty];
+          trans_sqsum[col * h + row] = sqsum_temp[tx][ty];
      }
 
 }
